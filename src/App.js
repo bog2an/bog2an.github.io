71f63f6cd7f2e6385475b645578bbab6167dd44b
Bog2an <bogdan-zaviruha@mail.ru>
2022-02-03 03:51:34 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useState} from "react";
+import {Grid} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    let k = 0;
+    let n = 0;
+    let masA = [];
+    let masNumbers = [];
+    const [output, setOutput] = useState('');
+
+
+    function isEven(question) {
+        return question % 2 === 0;
+    }
+
+    function sum(arr,  startIdx,  deep,  currSum) {
+        if (deep === 1){
+            for (let i = startIdx; i < arr.length; i++)
+                if (isEven(currSum + arr[i]))
+                    masNumbers.push(currSum + arr[i]);
+        } else {
+            for (let i = startIdx; i < arr.length; i++)
+                sum(arr, i + 1, deep - 1, currSum + arr[i]);
+        }
+    }
+
+    function activateButton() {
+        setOutput('');
+        masNumbers = [];
+        sum(masA, 0, k, 0);
+        return setOutput(String(Math.max.apply(null,masNumbers)))
+    }
+
+    function setMasA(event) {
+        masA.length=0;
+        masA = event.target.value.split(" ");
+        n = masA.length;
+        for (let i=0; i<n; i++)
+            masA[i] = Number(masA[i]);
+    }
+
+    function setK(event) {
+        k = event.target.value;
+    }
+
+    return (
+    <Box
+        component="form"
+        sx={{
+            '& > :not(style)': { m: 3, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+    >
+        <Grid
+            container
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <TextField sx={{ pb: 2 }} onInput={setMasA} label="Массив А - через пробел" variant="outlined"/>
+            <TextField onInput={setK} label="Целое число К" variant="outlined" />
+            <Button  sx={{ my: 2 }} onClick={activateButton} variant="contained">Вычислить</Button>
+            <TextField
+                disabled
+                label="Ответ"
+                value={output}
+            />
+        </Grid>
+    </Box>
   );
 }
 
